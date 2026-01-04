d2c88c3896238ef540f3278d1d539d7b1be86785
Shyam N <shyam1182006@gmail.com>
2026-01-04 18:18:39 +0530

    Baseline Vite React app

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>WANDERFEST TOURLINE</h1>
+      <p>Baseline deployment test</p>
+    </div>
+  );
+}
+
+export default App;
